0e476a8c826dc0388188f6aa3328b2359667cf01
HyunSeung, Shin <tlsgustm12@gmail.com>
2023-05-29 16:25:13 +0900

    test

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+
+export default function Page() {
+  return (
+    <div>
+      <h4 className="title">애플후레시</h4>
+      <p className="title-sub">by dev Shin</p>
+    </div>
+  )
+}
